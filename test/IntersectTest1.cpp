568040b6ca7d7d647a2342874fc6fe1183a82e3b
NNNNNF <17373532@buaa.edu.cn>
2020-03-09 20:44:41 +0800

    add test_higher

--- test/IntersectTest1.cpp
+++ test/IntersectTest1.cpp
@@ -75,5 +75,68 @@ namespace IntersectTest1
 			int num = points.size();
 			Assert::AreEqual(num,1);
 		}
+
+		TEST_METHOD(testMethod8) {
+			Point pt1(0, 0);
+			Point pt2(1, 1);
+			Point pt3(1, 0);
+			Point pt4(0, 1);
+			Point pt5(1, 2);
+			Point pt6(2, 0);
+			Line l1 = l1.getLine(pt1, pt2);
+			Line l2 = l2.getLine(pt3, pt4);
+			Line l3 = l3.getLine(pt5, pt6);
+			Point p1 = l1.getintersection(l1, l2);
+			Point p2 = l1.getintersection(l1, l3);
+			Point p3 = l2.getintersection(l2, l3);
+			set <Point> points;
+			points.insert(p1);
+			points.insert(p2);
+			points.insert(p3);
+			int num = points.size();
+			Assert::AreEqual(num, 3);
+		}
+
+		TEST_METHOD(testMethod9) {
+			Point pt1(0, 0);
+			Point pt2(1, 1);
+			Point pt3(0, 0);
+			Point pt4(0, 1);
+			Point pt5(0, 0);
+			Point pt6(-1, 0);
+			Line l1 = l1.getLine(pt1, pt2);
+			Line l2 = l2.getLine(pt3, pt4);
+			Line l3 = l3.getLine(pt5, pt6);
+			Point p1 = l1.getintersection(l1, l2);
+			Point p2 = l1.getintersection(l1, l3);
+			Point p3 = l2.getintersection(l2, l3);
+			set <Point> points;
+			points.insert(p1);
+			points.insert(p2);
+			points.insert(p3);
+			int num = points.size();
+			Assert::AreEqual(num, 1);
+		}
+
+		TEST_METHOD(testMethod10) {
+			Point pt1(100000, 100000);
+			Point pt2(-3, 19999);
+			Point pt3(99999, 10000);
+			Point pt4(4, 8366);
+			Point pt5(-6542, 768);
+			Point pt6(9999, -4);
+			Line l1 = l1.getLine(pt1, pt2);
+			Line l2 = l2.getLine(pt3, pt4);
+			Line l3 = l3.getLine(pt5, pt6);
+			Point p1 = l1.getintersection(l1, l2);
+			Point p2 = l1.getintersection(l1, l3);
+			Point p3 = l2.getintersection(l2, l3);
+			set <Point> points;
+			points.insert(p1);
+			points.insert(p2);
+			points.insert(p3);
+			int num = points.size();
+			Assert::AreEqual(num, 3);
+		}
 	};
 }
