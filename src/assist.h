edfb83f5c62067f4e2c036fb875b67b54839f6d4
NNNNNF <17373532@buaa.edu.cn>
2020-03-10 18:00:48 +0800

    add circle

--- src/assist.h
+++ src/assist.h
@@ -14,9 +14,7 @@ public:
 		Xpoint = x;
 		Ypoint = y;
 	}
-
 	string showPoint();
-
 	bool operator<(const Point& a)const
 	{
 		if (Xpoint != a.Xpoint) {
@@ -44,16 +42,15 @@ public:
 	double b = 0;
 	double c = 0;
 public:
+	//计算a,b,c
 	Line getLine(Point pt1, Point pt2);
+	//获得交点
 	Point getintersection(Line l1, Line l2);
+	//是否相交
 	bool ifinter(Line l1, Line l2);
-
-	//string showPoint(Point ptinter);
+	//单元测试
 	string showLine();
-
-	int getpoints(set<Line> lines);
-
-
+	//重载操作符
 	bool operator < (const Line& lx) const
 	{
 		if (a != lx.a) {
@@ -69,7 +66,7 @@ public:
 
 };
 
- inline Line Line::getLine(Point pt1, Point pt2) {
+inline Line Line::getLine(Point pt1, Point pt2) {
 	Line result;
 	/*
 	a = y1-y2
@@ -111,20 +108,81 @@ inline string Point::showPoint() {
 	string s = "(" + to_string((int)Xpoint) + "," + to_string((int)Ypoint) + ")";
 	return s;
 }
-//int Line::getpoints(set<Line> lines) {
-//	for (Line lx : lines) {
-//		if (lx.ifinter(lx, l)) {
-//			Point px = lx.getintersection(lx, l);
-//			//cout << px.Xpoint << px.Ypoint << endl;
-//			points.insert(px);
-//			//cout << points.size() << endl;
-//		}
-//	}
-//}
+
+
 inline Point getintersection(Line l1, Line l2) {
 	Point result;
 	double mid = l1.a * l2.b - l2.a * l1.b;
 	result.Xpoint = (l2.c * l1.b - l1.c * l2.b) / mid;
 	result.Ypoint = (l1.c * l2.a - l2.c * l1.a) / mid;
 	return result;
+}
+
+class Circle {
+public:
+	double Xpoint = 0;
+	double Ypoint = 0;
+	double r = 0;
+
+public:
+	Circle() {}
+	Circle(double x, double y,double r) {
+		Xpoint = x;
+		Ypoint = y;
+		r = r;
+	}
+	bool operator<(const Circle& a)const
+	{
+		if (Xpoint != a.Xpoint) {
+			return Xpoint < a.Xpoint;
+		}
+		else if (Ypoint != a.Ypoint) {
+			return Ypoint < a.Ypoint;
+		}
+		else {
+			return r<a.r;
+		}
+	}
+
+	bool operator == (const Circle& p) const
+	{
+		if (Xpoint != p.Xpoint) {
+			return false;
+		}
+		else if (Ypoint != p.Ypoint) {
+			return false;
+		}
+		else {
+			return r == p.r;
+		}
+	}
+
+	bool ccifinter(Circle c1, Circle c2);
+	bool clifinter(Circle c, Line l);
+	Line getCCline(Circle c1, Circle c2);
+	int getCLintersect(Circle c, Line l);
+	
+};
+
+bool Circle::ccifinter(Circle c1, Circle c2) {
+	double length = sqrt((c2.Ypoint - c1.Ypoint) * (c2.Ypoint - c1.Ypoint) + (c2.Xpoint - c1.Xpoint) * (c2.Xpoint- c1.Xpoint));
+	if (length > c1.r + c2.r || length < fabs(c1.r - c2.r))
+	{
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+Line Circle::getCCline(Circle c1, Circle c2) {
+	double a = 2 * (c2.Xpoint - c1.Xpoint);
+	double b = 2 * (c2.Ypoint - c1.Ypoint);
+	double c = -1.0*(c1.r * c1.r - c2.r * c2.r + c2.Xpoint * c2.Xpoint - c1.Xpoint * c1.Xpoint +
+		c2.Ypoint * c2.Ypoint - c1.Ypoint * c1.Ypoint);
+	Line l;
+	l.a = a;
+	l.b = b;
+	l.c = c;
+	return l;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,21 @@
 
 using namespace std;
 
+set <Point> points;
+
 int main(int argc, char** argv) {
+
 	set <Line> lines;
-	set <Point> points;
+	set <Circle> circles;
+
 	string inputFilename;
 	string outputFilename;
 
 	if (argv[1][0] == '-' && argv[1][1] == 'i' && argv[3][0] == '-' && argv[3][1] == 'o') {
 		inputFilename = argv[2];
 		outputFilename = argv[4];
-	}else if (argv[1][0] == '-' && argv[1][1] == 'o' && argv[3][0] == '-' && argv[3][1] == 'i') {
+	}
+	else if (argv[1][0] == '-' && argv[1][1] == 'o' && argv[3][0] == '-' && argv[3][1] == 'i') {
 		inputFilename = argv[4];
 		outputFilename = argv[2];
 	}
@@ -27,34 +32,97 @@ int main(int argc, char** argv) {
 	fin >> n;
 	for (i = 0; i < n; i++) {
 		char L;
-		//cin >> L;
-		//cout << L << endl;
 		int x1, y1, x2, y2;
-		fin >> L >> x1 >> y1 >> x2 >> y2;
-		Point pt1, pt2;
-		pt1.Xpoint = x1;
-		pt1.Ypoint = y1;
-		pt2.Xpoint = x2;
-		pt2.Ypoint = y2;
-		Line l = l.getLine(pt1, pt2);
-		for (Line lx : lines) {
-			if (lx.ifinter(lx, l)) {
-				Point px = lx.getintersection(lx, l);
-				//cout << px.Xpoint << px.Ypoint << endl;
-				points.insert(px);
-				//cout << points.size() << endl;
+		fin >> L ;
+		if (L == 'L') {
+			fin >> x1 >> y1 >> x2 >> y2;
+			Point pt1, pt2;
+			pt1.Xpoint = x1;
+			pt1.Ypoint = y1;
+			pt2.Xpoint = x2;
+			pt2.Ypoint = y2;
+			Line l = l.getLine(pt1, pt2);
+			for (Line lx : lines) {
+				if (lx.ifinter(lx, l)) {//判断是否平行
+					Point px = lx.getintersection(lx, l);//求交点
+					points.insert(px);//插入点集合
+				}
 			}
+			for (Circle c : circles) {
+				c.clifinter(c, l);
+			}
+			lines.insert(l);
 		}
-		lines.insert(l);
+		if (L == 'C') {
+			int cx, cy,cr;
+			fin >> cx >> cy >> cr;
+			Circle c;
+			c.Xpoint = cx;
+			c.Ypoint = cy;
+			c.r = cr;
+			for (Line lx : lines) {
+				c.clifinter(c, lx);
+			}
+			for (Circle cc : circles) {
+				if (c.ccifinter(c, cc)) {
+					Line l = c.getCCline(c, cc); 
+					c.clifinter(c, l);
+				}
+			}
+			circles.insert(c);
+		}
 	}
-	cout << points.size() << endl;
+	//cout << points.size() << endl;
 	out << points.size() << endl;
 	fin.close();
 	out.close();
 }
 
+bool Circle::clifinter(Circle c, Line l) {
+	double cx = c.Xpoint;
+	double cy = c.Ypoint;
+	double cr = c.r;
+	double la = l.a;
+	double lb = l.b;
+	double lc = l.c;
+	double d = fabs(la * cx + lb * cy + lc) / sqrt(la * la + lb * lb);
+	if (d > c.r) {
+		return false;
+	}
+	else if(lb==0){
+		double parax = -1.0 * lc / la;
+		double paray = cr * cr - cx * cx - (lc * lc + 2 * lc * la * cx) / (la * la);
+		Point pt1;
+		Point pt2;
+		pt1.Xpoint = parax;
+		pt1.Ypoint = cy + sqrt(paray);
+		pt2.Xpoint = parax;
+		pt2.Ypoint = cy - sqrt(paray);
+		points.insert(pt1);
+		points.insert(pt2);
+		//cout << pt1.Xpoint << " " << pt1.Ypoint << endl;
+		//cout << pt2.Xpoint << " " << pt2.Ypoint << endl;
+	}
+	else {
+		double paraa = la * la + lb * lb;
+		double parab = -2.0 * lb * lb * cx + 2 * la * lc + 2 * la * lb * cy;
+		double parac = lb * lb * cx * cx + (lc + cy * lb) * (lc + cy * lb) - cr * cr * lb * lb;
+		double deta = sqrt(parab * parab - 4 * paraa * parac);
+		Point pt1;
+		Point pt2;
+		pt1.Xpoint = (-1.0 * parab + deta) /( 2 * paraa);
+		pt1.Ypoint = (-1.0 * la * pt1.Xpoint - lc) / lb;
+		pt2.Xpoint = (-1.0 * parab - deta) /( 2 * paraa);
+		pt1.Ypoint = (-1.0 * la * pt2.Xpoint - lc) / lb;
+		points.insert(pt1);
+		points.insert(pt2);
+		//cout << pt1.Xpoint << " " << pt1.Ypoint << endl;
+		//cout << pt2.Xpoint << " " << pt2.Ypoint << endl;
+	}
+	return true;
+}
 /*
 	   char ** argv 表示main函数所在的程序在运行时输入的参数表，用,或者空格分隔：
-	   第一个参数存在 argv[1]中 每个第二维数组单元存一位数，例如 biji 12546 
+	   第一个参数存在 argv[1]中 每个第二维数组单元存一位数，例如 biji 12546
 	   即argv[1][0] = 1, argv[1][2]=2, argv[1][3]=5, argv[1][4]=5, argv[1][6]=6, 而argv[1]=12546
 */
